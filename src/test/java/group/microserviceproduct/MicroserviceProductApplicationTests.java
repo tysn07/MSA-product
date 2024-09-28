336f0254e60cda989d2762227c86c3d631ad2a6c
tysn07 <tyler0702@naver.com>
2024-09-28 14:42:38 +0900

    init

---
+++ src/test/java/group/microserviceproduct/MicroserviceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package group.microserviceproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
