342d44906159082b7bfdff93f411edbb1c160d44
tysn07 <tyler0702@naver.com>
2024-10-20 13:18:23 +0900

    :recycle: edit cross origin

--- src/main/java/group/microserviceproduct/global/config/CORSConfig.java
+++ src/main/java/group/microserviceproduct/global/config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-            .allowedOrigins("http://15.165.174.43:8081","http://15.165.174.43:8082","http://3.39.43.179:8084") // 허용할 오리진
+            .allowedOrigins("http://15.165.174.43:8081","http://15.165.174.43:8082","http://3.39.43.179:8084","https://son7shop.com") // 허용할 오리진
             .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
             .allowedHeaders("Authorization", "Content-Type") // 허용할 헤더
             .allowCredentials(true) // 쿠키를 포함할지 여부
